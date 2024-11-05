ce35b2d2a0f6f935d82a6c8b439ffcba36d8e6b4
Ngô Anh Tuấn <138982782+tuanit03@users.noreply.github.com>
2024-11-05 14:00:27 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,10 @@
-// config.js
-export default window._config;
+window._config = {
+    cognito: {
+        userPoolId: process.env.REACT_APP_USER_POOL_ID,
+        userPoolClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+        region: process.env.REACT_APP_REGION
+    },
+    api: {
+        url: process.env.REACT_APP_API_URL
+    }
+};
