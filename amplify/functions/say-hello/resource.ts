129317deda972a3d8df5d8ee08b70a6e403cedd7
Ngô Anh Tuấn <138982782+tuanit03@users.noreply.github.com>
2024-11-05 15:01:14 +0700

    Update resource.ts

--- amplify/functions/say-hello/resource.ts
+++ amplify/functions/say-hello/resource.ts
@@ -3,11 +3,11 @@ import { defineFunction } from '@aws-amplify/backend';
 export const sayHello = defineFunction({
   environment: {
     NAME: "World",
-    REACT_APP_USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID
-    REACT_APP_USER_POOL_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID
-    REACT_APP_REGION: process.env.REACT_APP_REGION
+    REACT_APP_USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+    REACT_APP_USER_POOL_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+    REACT_APP_REGION: process.env.REACT_APP_REGION,
     REACT_APP_API_URL: process.env.REACT_APP_API_URL
-  }
+  },
   // optionally specify a name for the Function (defaults to directory name)
   name: 'say-hello',
   // optionally specify a path to your handler (defaults to "./handler.ts")
