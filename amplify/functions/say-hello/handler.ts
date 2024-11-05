34fc9edbb1978bd30b011e2f3631d79acf3921b6
Ngô Anh Tuấn <138982782+tuanit03@users.noreply.github.com>
2024-11-05 15:00:35 +0700

    Update handler.ts

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -3,5 +3,5 @@ import { env } from '$amplify/env/say-hello'; // the import is '$amplify/env/<fu
 
 export const handler: Handler = async (event, context) => {
   // your function code goes here
-  return ${env.REACT_APP_USER_POOL_ID}, ${env.REACT_APP_USER_POOL_CLIENT_ID}, ${env.REACT_APP_REGION}, ${env.REACT_APP_API_URL};
+  return `${env.REACT_APP_USER_POOL_ID}, ${env.REACT_APP_USER_POOL_CLIENT_ID}, ${env.REACT_APP_REGION}, ${env.REACT_APP_API_URL}`;
 };
