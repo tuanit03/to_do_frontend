adbb4f73007c08b00099ce110f03d2ae157e64df
Ngô Anh Tuấn <138982782+tuanit03@users.noreply.github.com>
2024-11-05 12:58:09 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,2 @@
-window._config = {
-    cognito: {
-        userPoolId: process.env.USER_POOL_ID,
-        userPoolClientId: process.env.USER_POOL_CLIENT_ID,
-        region: process.env.REGION
-    },
-    api: {
-        url: process.env.API_URL
-    }
-};
+// config.js
+export default window._config;
